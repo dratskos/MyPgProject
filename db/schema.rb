fedac38eb3e40f040cb2eb6ec5d0b1a309639c99
DevBox <thanos.patrikas@gmail.com>
2020-04-24 15:43:26 +0300

    Add Data Relationships...*

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_22_174625) do
+ActiveRecord::Schema.define(version: 2020_04_24_122057) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,7 +21,10 @@ ActiveRecord::Schema.define(version: 2020_04_22_174625) do
     t.datetime "updated_at", precision: 6, null: false
     t.text "body"
     t.string "slug"
+    t.integer "status", default: 0
+    t.bigint "topic_id"
     t.index ["slug"], name: "index_blogs_on_slug", unique: true
+    t.index ["topic_id"], name: "index_blogs_on_topic_id"
   end
 
   create_table "friendly_id_slugs", force: :cascade do |t|
@@ -53,4 +56,11 @@ ActiveRecord::Schema.define(version: 2020_04_22_174625) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "topics", force: :cascade do |t|
+    t.string "title"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  add_foreign_key "blogs", "topics"
 end

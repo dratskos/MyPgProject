4d51c410f6f8ec596fc81d1bfaee57e50333f0d2
DevBox <thanos.patrikas@gmail.com>
2020-04-25 20:14:54 +0300

    Another changes to Implemented skill, portfolio, blog

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,17 +1,6 @@
 class Blog < ApplicationRecord
-<<<<<<< HEAD
 	enum status: { draft: 0, published: 1}
 	extend FriendlyId
 	friendly_id :title, use: :slugged
 	validates_presence_of :title, :body 
-=======
-	enum status: { draft: 0, published: 1 }
-	extend FriendlyId
-	friendly_id :title, use: :slugged
-
-	validates_presence_of :title, :body
-
-	belongs_to :topic 
-
->>>>>>> 17011b5b02256be5a0d323f3daae0f210f4cd472
 end
--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,7 +1,3 @@
 class Portfolio < ApplicationRecord
-<<<<<<< HEAD
-=======
-
->>>>>>> 17011b5b02256be5a0d323f3daae0f210f4cd472
 	validates_presence_of :title, :body, :main_image, :thumb_image
 end

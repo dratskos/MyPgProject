17011b5b02256be5a0d323f3daae0f210f4cd472
DevBox <thanos.patrikas@gmail.com>
2020-04-25 10:42:04 +0300

    Changing the bin files

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -7,18 +7,18 @@ class PortfoliosController < ApplicationController
 		@portfolio_item=Portfolio.new
 	end
 
-	def show
-		@portfolio_item=Portfolio.find(params[:id])
-	end
 
 
 	def create
+
     	@portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
 
     	respond_to do |format|
       	if @portfolio_item.save
+
        	 format.html { redirect_to portfolios_path, notice: 'Your portfolio item is now live.' }
         else
+
         format.html { render :new }
         end
     end
@@ -41,6 +41,10 @@ class PortfoliosController < ApplicationController
         
       end
     end
+  end
+
+  def show
+    @portfolio_item = Portfolio.find(params[:id])
   end
 
   def destroy

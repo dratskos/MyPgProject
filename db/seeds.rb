efcd1fa29c035308c2c8cee2dca4f7b2f08210bb
DevBox <thanos.patrikas@gmail.com>
2020-04-26 13:59:55 +0300

    Integrated technologies model

--- db/seeds.rb
+++ db/seeds.rb
@@ -46,5 +46,12 @@ end
     thumb_image: "http://placehold.it/350x200"
   )
 end
-
 puts "9 portfolio items created"
+
+
+3.times do |technology|
+  Portfolio.last.technologies.create!(
+    name: "Technology #{technology}"
+  )
+end
+puts "3 technologies items created"
